d685dd2e88525fb49d6f9ad19ea2f2f302768f22
Zaur <ironetz@rambler.ru>
2021-03-04 18:02:31 +0300

    lesson 26 insertBefore

--- lesson25/js/script.js
+++ lesson25/js/script.js
@@ -1,2 +1,38 @@
 'use strict';
 
+const box = document.getElementById('box'),
+    btns = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    hearts = document.querySelectorAll('.heart '),
+    wrapper = document.querySelector('.wrapper');
+
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
+
+box.style.cssText = 'background-color: green; width: 200px';
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+for (let i = 0; i < hearts.length; i++){
+    hearts[i].style.backgroundColor = 'black';
+}
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'blue';
+});
+
+const div = document.createElement('div');
+const text = document.createTextNode('ПРивет, мир!');
+
+div.classList.add('black');
+wrapper.insertBefore(div, hearts[0]);
+// wrapper.appendChild(div);
+// document.body.append(div);
+//wrapper.append(div);
+// wrapper.prepend(div);
+// hearts[0].before(div);
+// hearts[0].after(div);
+// circles[0].remove();
+// hearts[0].replaceWith(circles[0]);
+
